49e9d126121f0a677c7a82c2c6afc5cc869c47d6
noahw <noahbweiner@gmail.com>
2016-07-29 15:02:37 -0700

    Implement sweet admin dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,8 @@ class HomeController < ApplicationController
       elsif user.is_student?
         redirect_to '/student'
       end
+    else
+      redirect_to new_user_session_path
     end
   end
 end
