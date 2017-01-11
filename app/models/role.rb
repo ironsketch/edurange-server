567a83e2d54a7b90ce030329e8e11865b5fd3154
noahw <noahbweiner@gmail.com>
2017-01-11 15:57:58 -0800

    Fix bug in Role validations (and refactor the heck out of it)

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,79 +3,52 @@ class Role < ActiveRecord::Base
   has_many :role_recipes, dependent: :destroy
   has_many :recipes, through: :role_recipes
   has_many :instance_roles, dependent: :destroy
+  has_many :instances, through: :instance_roles
   has_one :user, through: :scenario
 
   serialize :packages, Array
 
-  validates :name, presence: true, uniqueness: { scope: :scenario, message: "Name taken" }
+  validates :name, presence: true, uniqueness: { scope: :scenario }
   validate :instances_stopped
 
-  before_destroy :instances_stopped, prepend: :true
+  before_destroy :instances_stopped?, prepend: :true
   after_destroy :update_scenario_modified
 
   def instances_stopped
-    self.instance_roles.each do |instance_role|
-      instances = []
-      if not instance_role.instance.stopped?
-        instances << instance_role.instance.name
-      end
-      if instances.size > 0
-        errors.add(:running, "the following instances using this role must be stopped before deletion or modification of role. #{instances.to_s}")
-        return false
-      end
+    unless instances_stopped?
+      errors.add(:running, "the following instances using this role must be stopped"\
+                           "before deletion or modification of role. #{instances.to_s}")
     end
-    if self.scenario.modifiable?
-      self.scenario.update(modified: true)
-    end
-    true
   end
 
   def instances_stopped?
-    self.instance_roles.each do |instance_role|
-      if not instance_role.instance.stopped?
-        return false
-      end
-    end
-    true
+    instances.all? { |instance| instance.stopped? }
   end
 
   def update_scenario_modified
-    if self.scenario.modifiable?
-      self.scenario.update_attribute(:modified, true)
-    end
-    true
+    scenario.update_attribute(:modified, true) if scenario.modifiable?
   end
 
   def package_add(name)
     if name.class != String or name == ''
       errors.add(:packages, 'package must be non blank String')
-      return false
-    end
-    if self.packages.include? name
+    elsif self.packages.include? name
       errors.add(:packages, "package already exists")
-      return false
     else
-      self.update(packages: packages << name )
-      if self.errors.any?
-        return false
-      end
+      update(packages: packages << name )
     end
-    true
+
+    return errors.empty?
   end
 
   def package_remove(name)
     if not self.packages.include? name
       errors.add(:packages, "package does not exist")
-      return false
     else
-      packages = self.packages
-      packages.delete(name)
-      self.update(packages: packages)
-      if self.errors.any?
-        return false
-      end
+      # I swear this works: try `["one", "two", "three"] - ["two"]` in a ruby console
+      # (it outputs `["one", "three"]`
+      update(packages: packages - [name])
     end
-    true
+    return errors.empty?
   end
-
 end
