4d4b10ecfa0712d5f2a8d55e6e7545e8ddce8f75
noahw <noahbweiner@gmail.com>
2017-01-11 16:17:17 -0800

    Flesh out scenario spec

--- spec/models/scenario_spec.rb
+++ spec/models/scenario_spec.rb
@@ -10,9 +10,11 @@ describe Scenario do
       expect(build(:scenario, user: nil)).to be_invalid
     end
 
-    it "is invalid when the path doesn't exist" do
-      expect(build(:scenario, name: "nonexistant")).to be_invalid
-      expect(build(:scenario, location: :production)).to be_invalid
+    describe "#path" do
+      it "is invalid when it doesn't exist" do
+        expect(build(:scenario, name: "nonexistant")).to be_invalid
+        expect(build(:scenario, location: :production)).to be_invalid
+      end
     end
 
     describe "#name" do
@@ -27,20 +29,40 @@ describe Scenario do
   end
 
   describe "#load" do
-    describe "with valid YAML" do
+    describe "with scenario 'test1'" do
       before :all do
         @scenario = create(:scenario, name: "Test1", location: :test)
       end
 
-      it "to be valid" do
+      it "is valid" do
         expect(@scenario).to be_valid
       end
 
-      it "creates roles" do
-        expect(@scenario.roles.count).to be(1)
-        expect(@scenario.roles.first).to be_valid
-        expect(@scenario.roles.first.packages.count).to be(2)
-        expect(@scenario.roles.first.recipes.count).to be(2)
+      describe "roles" do
+        it "creates one 1 role" do
+          expect(@scenario.roles.count).to eq(1)
+        end
+
+        it "assigns 2 packages and 2 recipes to the role" do
+          expect(@scenario.roles.first.packages.count).to eq(2)
+          expect(@scenario.roles.first.recipes.count).to eq(2)
+        end
+      end
+
+      it "creates 2 groups" do
+        expect(@scenario.groups.count).to eq(2)
+      end
+
+      it "creates 1 cloud" do
+        expect(@scenario.clouds.count).to eq(1)
+      end
+
+      it "creates 1 subnet" do
+        expect(@scenario.subnets.count).to eq(1)
+      end
+
+      it "creates 1 instance" do
+        expect(@scenario.instances.count).to eq(1)
       end
     end
   end
