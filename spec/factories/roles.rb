22c59158bf43e66d8033353aa6be04e663939c06
noahw <noahbweiner@gmail.com>
2017-01-11 16:58:25 -0800

    Write some Role specs

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -2,8 +2,14 @@
 
 FactoryGirl.define do
   factory :role do
-    name "MyString"
-    packages "MyString"
-    recipes "MyString"
+    name { Faker::Name.first_name }  # Roles named after people? Why not?
+
+    scenario do
+      if Scenario.count > 1
+        Scenario.first
+      else
+        create(:scenario)
+      end
+    end
   end
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 describe Role do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    @scenario = create(:scenario)
+  end
+
+  it "has a valid factory" do
+    expect(build(:role)).to be_valid
+  end
+
+  describe "validations" do
+    describe "#name" do
+      it "is invalid when blank" do
+        expect(build(:role, name: "")).to be_invalid
+      end
+
+      it "is invalid when not unique" do
+        role = create(:role, name: "good roll", scenario: @scenario)
+        expect(role).to be_valid
+        expect(build(:role, name: "good roll", scenario: @scenario)).to be_invalid
+        role.destroy!
+      end
+    end
+
+    describe "#scenario" do
+      it "is invalid when not owned by a scenario" do
+        expect(build(:role, scenario: nil)).to be_invalid
+      end
+    end
+  end
 end
